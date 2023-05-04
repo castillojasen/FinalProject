814cca6f74fdafe6ad347576796bcd4aeb8afaed
Jasen Castillo <castillojasen@csu.fullerton.edu>
2023-05-03 22:09:09 -0700

    first

---
+++ FinalProject/AddView.swift
@@ -0,0 +1,52 @@
+//
+//  AddView.swift
+//  FinalProject
+//
+//  Created by Jasen Castillo on 5/3/23.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @ObservedObject var transactions: Transactions
+    @Environment(\.dismiss) var dismiss
+
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+
+    let types = ["Business", "Personal"]
+
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = TransactionItem(name: name, type: type, amount: amount)
+                    transactions.items.append(item)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(transactions: Transactions())
+    }
+}
+
+
